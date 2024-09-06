91cf20052d5a04707bf7e9edc6e67900f8a43a2c
EnochAdenaya <enochadenaya16@gmail.com>
2024-09-06 10:54:56 +0100

    pizza template

--- pizza.js
+++ pizza.js
@@ -1,36 +1,36 @@
 // Script for the food menu
-         var btn_pizza = document.getElementById("btn_pizza");
-         var btn_salads = document.getElementById("btn_salads");
-         var btn_starter = document.getElementById("btn_starter");
-         var pizza = document.getElementById("pizza");
-         var salads = document.getElementById("salads");
-         var starter = document.getElementById("starter");
+const btn_pizza = document.getElementById("btn_pizza");
+const btn_salads = document.getElementById("btn_salads");
+const btn_starter = document.getElementById("btn_starter");
+const pizza = document.getElementById("pizza");
+const salads = document.getElementById("salads");
+const starter = document.getElementById("starter");
  
-        btn_pizza.addEventListener('click', ()=>{
-            pizza.style.display='block';
-            salads.style.display='none';
-            starter.style.display='none';
-        })
+btn_pizza.addEventListener('click', ()=>{
+    pizza.style.display='block';
+    salads.style.display='none';
+    starter.style.display='none';
+})
 
-        btn_salads.addEventListener('click', ()=>{
-            pizza.style.display='none';
-            salads.style.display='block';
-            starter.style.display='none';
-        })
+btn_salads.addEventListener('click', ()=>{
+    pizza.style.display='none';
+    salads.style.display='block';
+    starter.style.display='none';
+})
 
-        btn_starter.addEventListener('click', ()=>{
-            pizza.style.display='none';
-            salads.style.display='none';
-            starter.style.display='block';
-        })
+btn_starter.addEventListener('click', ()=>{
+    pizza.style.display='none';
+    salads.style.display='none';
+    starter.style.display='block';
+})
 
-        // Add active class to the current button (highlight it)
-        var header = document.getElementById("buttons");
-        var btns = header.getElementsByClassName("btn");
-        for (var i = 0; i < btns.length; i++) {
-          btns[i].addEventListener("click", function() {
-          var current = document.getElementsByClassName("active");
-          current[0].className = current[0].className.replace(" active", "");
-          this.className += " active";
-          });
-        }
+// Add active class to the current button (highlight it)
+const header = document.getElementById("buttons");
+const btns = header.getElementsByClassName("btn");
+for (i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+    current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+    });
+}
